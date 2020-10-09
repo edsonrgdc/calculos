ba696c42f434d5fe4c61956055566ec0dc797910
edsonrgdc18 <edsonrgdc18@gmail.com>
2020-10-09 17:27:45 -0300

    commit -m "calculosjs"

---
+++ calculos.js
@@ -0,0 +1,35 @@
+
+function areaTriângulo() { return (b*h)/2} area()
+
+console.log('resultado', area())
+
+
+function mediaAritmetica() { return (5+5+5+5)/4} mediaAritmetica()
+
+console.log('resultado', mediaAritmetica())
+
+
+function areaCirculo() { return 3.14*r**2 } areaCirculo()
+
+console.log('resultado', areaCirculo())
+
+
+function movimentoRuv() { return V = Vo + (a**2) * t } movimentoRuv()
+
+console.log('resultado', movimentoRuv())
+
+
+function movimentoRuv() { return Vm = S / t } movimentoRuv()
+
+console.log('resultado', movimentoRu())
+
+
+pessoa = { 
+  nome: 'Edson',
+  idade: 18,
+  vivo: true,
+  falar: function (jogar) {
+    alert (jogar)
+  }
+}
+objetoVazio = {}
